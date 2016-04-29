08a2688d8b876fb1ad59992d96ce512c03bdbd84
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-04-29 16:58:46 +0800

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>NSI :: Employee Form Submission</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ URL::to('/') }}/bootstrap-3.3.6-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+</head>
+<body id="app-layout">
+
+    @yield('content')
+
+    <!-- JavaScripts -->
+    <script src="{{ URL::to('/') }}/bootstrap-3.3.6/dist/js/jquery.min.js"></script>
+    <script src="{{ URL::to('/') }}/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
